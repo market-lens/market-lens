64f5133dee5ae528a1dac98a7850b37c9c4196ae
Brandon Ling <79050338+bzzling@users.noreply.github.com>
2024-12-20 23:55:16 -0500

    firebase update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBVEYvzes5w2jnNYzeZuDoQ0DgqqKC_zis",
   authDomain: "market-lens-abd56.firebaseapp.com",
@@ -19,5 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Conditionally initialize Firebase Analytics
+isSupported().then((supported) => {
+  if (supported) {
+    const analytics = getAnalytics(app);
+  }
+});
+
 export const auth = getAuth(app);
